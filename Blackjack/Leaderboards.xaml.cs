75eeaed792cb039d86938b4a6cfe9ed2d94d8b1e
Vc2004y <79786184+Vc2004y@users.noreply.github.com>
2021-06-18 19:43:42 +1000

    Added Stastic Printing Functionality

--- Blackjack/Leaderboards.xaml.cs
+++ Blackjack/Leaderboards.xaml.cs
@@ -30,25 +30,6 @@ namespace Blackjack
             dgLeaders.ItemsSource = lb.DefaultView;
         }
 
-        private void BtnRefresh_OnClick(object sender, RoutedEventArgs e)
-        {
-            DataTable lb = new DataTable();
-            using (MySqlConnection conn = new MySqlConnection(connectionstring))
-            {
-                conn.Open();
-                string query = "SELECT Username, Points, Level, Money FROM user_data ORDER BY Points DESC";
-                using (MySqlDataAdapter da = new MySqlDataAdapter(query, conn))
-                    da.Fill(lb);
-            }
-
-            dgLeaders.ItemsSource = lb.DefaultView;
-
-            dgLeaders.Columns[0].Width = 120;
-            dgLeaders.Columns[1].Width = 100;
-            dgLeaders.Columns[2].Width = 200;
-            dgLeaders.Columns[3].Width = 200;
-        }
-
         private void dgLeaders_Loaded(object sender, RoutedEventArgs e)
         {
             dgLeaders.Columns[0].Width = 120;
--- Blackjack/Statistics.xaml.cs
+++ Blackjack/Statistics.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows;
 using System;
+using System.Drawing;
+using System.Drawing.Printing;
+using System.Printing;
 using Blackjack.Classes;
 using static Blackjack.Classes.Variables;
 
@@ -7,13 +10,15 @@ namespace Blackjack
 {
     public partial class Statistics : Window
     {
+        private string WR;
         public Statistics()
         {
             InitializeComponent();
 
             txtUser.Text = User;
             txtLR.Text = Losses.ToString();
-            txtWRR.Text = (Convert.ToDecimal(Wins) / Convert.ToDecimal(GamesPlayed) * 100m).ToString("##.##") + "%";
+            WR = (Convert.ToDecimal(Wins) / Convert.ToDecimal(GamesPlayed) * 100m).ToString("##.##") + "%";
+            txtWRR.Text = WR;
             txtWR1.Text = Wins.ToString();
             txtGPR.Text = GamesPlayed.ToString();
             txtMWR.Text = $"${MoneyWon}";
@@ -27,6 +32,29 @@ namespace Blackjack
             Home_Page hp = new Home_Page();
             hp.Show();
             Close();
+        }
+
+        private void BtnPrint_OnClick(object sender, RoutedEventArgs e)
+        {
+            string Statistics = $"Username: {User} \r\n Losses: {Losses} \r\n Wins: {Wins} \r\n Win Rate: {WR} \r\n Games Played: {GamesPlayed} \r\n Money Won: ${MoneyWon} \r\n Money Lost: {MoneyLost} \r\n Pushes: {Pushes} \r\n Cards Drawn: {CardsDrawn}";
+
+            PrintDocument StatsDoc = new PrintDocument();
+
+            StatsDoc.PrintPage += delegate(object sender1, PrintPageEventArgs e1)
+            {
+                e1.Graphics.DrawString(Statistics, new Font("Times New Roman", 12), new SolidBrush(Color.Black),
+                    new RectangleF(0, 0, StatsDoc.DefaultPageSettings.PrintableArea.Width,
+                        StatsDoc.DefaultPageSettings.PrintableArea.Height));
+            };
+
+            try
+            {
+                StatsDoc.Print();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"There was an error when printing. Error:{ex}");
+            }
         }
     }
 }
